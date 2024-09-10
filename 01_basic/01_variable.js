85d7144bfe8facb1d814961faeec76114e6b3114
Shubham Pandey <119473042+shubh895@users.noreply.github.com>
2024-09-10 14:20:28 +0000

    commit config done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 1444533;
+let accountEmail = "shubhampandey0134@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+  
+//  not allowed accountId = 2;
+
+accountEmail="rajat@gmail.com";
+accountPassword = "39329329832";
+accountCity = "Benguluru";
+// console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity ]);
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,31 @@
+// "use strict"; // treat all Js code as newer version
+
+// alert(3+3); we can using nodejs not a browser
+
+// console.log(3+3);
+// console.log("Shubham");
+
+let  names = "Shubham pandey";
+let age = 18;
+let isLoggedIn = false;
+let state;
+let number = null;
+
+
+// number => 2 to power 53
+// bigint 
+// string
+// boolean
+
+// null => standalone value 
+// undefined =>in which if we not assigned any value
+// symbol => unique
+
+// object
+
+console.table([age, isLoggedIn,state,number,names]);
+console.log(typeof(age) );
+console.log(typeof(isLoggedIn));
+
+
+
